a270b15839ab7c9c5f8bf6e335223ea9d33152a9
jaiku <jaiku7867@gmail.com>
2024-03-05 00:49:51 +0530

    In Progress

---
+++ script.js
@@ -0,0 +1,11 @@
+const form =document.querySelector(".add")
+let transactions =[];
+form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    const transaction ={
+        source :form.source.value,
+        amount:form.amount.value
+    };
+    transactions.push(transaction)
+    localStorage.setItem("transactions",JSON.stringify(transactions))
+})
