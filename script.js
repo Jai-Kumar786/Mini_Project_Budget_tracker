d6a04959c1cdfecccaa09023b669af3f181dc6a3
jaiku <jaiku7867@gmail.com>
2024-03-05 23:55:28 +0530

    Phase 1 completed

--- script.js
+++ script.js
@@ -1,11 +1,100 @@
 const form =document.querySelector(".add")
-let transactions =[];
-form.addEventListener("submit", (event)=>{
-    event.preventDefault();
+
+const incomeList =document.querySelector("ul.income-list")
+const expenseList=document.querySelector("ul.expense-list")
+const balance=document.getElementById("balance")
+const income =document.getElementById("income")
+const expense =document.getElementById("expense")
+
+
+let transactions =localStorage.getItem("transactions")!==null ? JSON.parse(localStorage.getItem("transactions")) :[];
+function updatedStatistics(){
+    const updatedIncome=transactions.filter(transaction=>{
+        return transaction.amount>0
+    }).reduce((total,transaction)=>total+=transaction.amount,0);
+
+    const updatedExpense=transactions.filter(transaction=>{
+        return transaction.amount<0
+    }).reduce((total,transaction)=> total+=Math.abs(transaction.amount),0)
+
+    updatedBalance = updatedIncome - updatedExpense;
+    balance.textContent=updatedBalance;
+    income.textContent=updatedIncome;
+    expense.textContent=updatedExpense;
+}
+updatedStatistics();
+
+
+function addTransaction(source,amount){
+    const time=new Date()
     const transaction ={
-        source :form.source.value,
-        amount:form.amount.value
+        id:Math.floor(Math.random()*100000),
+        source :source,
+        amount:amount,
+        time:`${time.toLocaleTimeString()} ${time.toLocaleDateString()}`
     };
     transactions.push(transaction)
     localStorage.setItem("transactions",JSON.stringify(transactions))
+    addTransactionDom(transaction.id,source,amount,transaction.time)
+}
+function generateTemplate(id,source,amount,time){
+    return `<li data-id="${id}">
+                                <p>
+                                    <span>${source}</span>
+                                    <span id="time_income">${time}</span>
+                                </p>
+                                $<span>${Math.abs(amount)}</span>
+                                <i class="bi bi-trash delete"></i>
+                            </li>`
+}
+function addTransactionDom(id,source,amount,time){
+    if(amount>0){
+       incomeList.innerHTML+= generateTemplate(id,source,amount,time);
+    }
+    else{
+        expenseList.innerHTML+=generateTemplate(id,source,amount,time)
+    }
+}
+form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    if(form.source.value.trim()===""|| form.amount.value===""){
+        return alert("Please add proper value")
+    }
+    addTransaction(form.source.value.trim(),Number( form.amount.value))
+    updatedStatistics();
+     form.reset()
+});
+
+
+function getTransaction(){
+    transactions.forEach(transaction=>{
+        if(transaction.amount>0){
+            incomeList.innerHTML+= generateTemplate(transaction.id,transaction.source,transaction.amount,transaction.time)
+        }
+        else{
+            expenseList.innerHTML+=generateTemplate(transaction.id,transaction.source,transaction.amount,transaction.time)
+        }
+    })
+}
+getTransaction();
+function deleteTransaction(id){
+    transactions=  transactions.filter(transaction =>{
+          return transaction.id !== id;
+      })
+    localStorage.setItem('transactions',JSON.stringify(transactions))
+}
+incomeList.addEventListener("click",event=>{
+    if(event.target.classList.contains("delete")){
+             event.target.parentElement.remove();
+             deleteTransaction( Number(event.target.parentElement.dataset.id));
+        updatedStatistics();
+    }
 })
+expenseList.addEventListener("click",event=>{
+    if(event.target.classList.contains("delete")){
+        event.target.parentElement.remove();
+        deleteTransaction( Number(event.target.parentElement.dataset.id));
+        updatedStatistics();
+    }
+})
+
